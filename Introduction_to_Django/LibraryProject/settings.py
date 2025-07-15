2f097fb3e1d3eb3a4d557de69404f8c88ebe9dac
Lerato Lerama <leratolerama3@gmail.com>
2025-07-15 13:37:06 +0200

    Add full Django project including bookshelf app

--- Introduction_to_Django/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--&=(o!%m3yn9n#8r^sjkwi60j)&be9t%#0=0%mer&^!(=m)8$%'
+SECRET_KEY = 'django-insecure-^nrikavdt*b)7qa@#s()sfu*z6xkn)(^qlzx7ca%x8*@cj!(4$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
