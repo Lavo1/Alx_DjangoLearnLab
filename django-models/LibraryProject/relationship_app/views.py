39ca4975b34d2e2ef9ef82d07b02800a907e5bec
Lavo1 <leratolerama3@gmail.com>
2025-07-21 17:45:58 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,2 @@
 # relationship_app/views.py
-from django.shortcuts import render
-from .models import Book
 
-def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
