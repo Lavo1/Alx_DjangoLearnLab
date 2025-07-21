4231e15c0cf293fd1967f3fa84c6c2370fba7b60
Lerato Lerama <leratolerama3@gmail.com>
2025-07-21 18:00:44 +0200

    Final fixes before push

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,2 +1,19 @@
 # relationship_app/views.py
 
+from django.shortcuts import render
+from .models import Book
+
+# Function-Based View
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+    
+from django.views.generic.detail import DetailView
+from .models import Library
+
+# Class-Based View
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
