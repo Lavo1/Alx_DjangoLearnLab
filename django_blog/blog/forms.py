a947d750b8d3895a0c4c403dd295781dc40c2636
Lerato Lerama <leratolerama3@gmail.com>
2025-08-17 18:18:59 +0200

    fix blog/forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,15 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post
+from .models import Comment
 
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea, max_length=1000, required=True)
+
+    class Meta:
+        model = Comment
+        fields = ['content']
+        
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
