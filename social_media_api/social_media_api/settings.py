920291832fb8cb0d66c9ebe1bc825c304bf7cbf5
Lerato Lerama <leratolerama3@gmail.com>
2025-08-19 15:17:07 +0200

    Implementing Posts and Comments Functionality

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
+    'posts',  
 ]
 AUTH_USER_MODEL = 'accounts.CustomUser' 
 
@@ -124,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend', 'rest_framework.filters.SearchFilter'],
+}
